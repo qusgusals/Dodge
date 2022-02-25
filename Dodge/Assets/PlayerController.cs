33654edc8f4d03182e18b9df53685b29a0f7411c
강동윤 <davidkang8111@gmail.com>
2022-02-25 17:30:19 +0900

    20220225

--- Dodge/Assets/PlayerController.cs
+++ Dodge/Assets/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // 이동에 사용할 리지드바디 컴포넌트
     public Rigidbody playerRigidbody;
     // 이동 속력
-    float speed = 3f;
+    public float speed = 3f;
 
     // 내 자신을 담을 변수
     public GameObject my;
@@ -28,6 +28,17 @@ public class PlayerController : MonoBehaviour
         float zInput = Input.GetAxis("Vertical");
         // 키보드 : 'W', ^ : 양의방향 : +1.0f
         // 키보드 : 'S', ▼ : 음의방향 : -1.0f
+
+        // 실제 이동 속도를 입력값과
+        // 이동 속력을 사용해 결정
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        // Vector3 속도를 (xSpeed, 0f, zSpeed) 생성
+        Vector3 newVelocity = new Vector3(xSpeed, 0, zSpeed);
+        // 리지디바디의 속도에 newVelocity 할당
+        playerRigidbody.velocity = newVelocity;
+
     }
 
     void DirectInput()
@@ -61,7 +72,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void Die()
+    public void Die()
     {
         my.SetActive(false);
     }
